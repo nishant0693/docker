52f9937182fc90e0950c0a3822eda5fd8284b032
nishant0693 <nishant.0693@gmail.com>
2024-01-07 20:44:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a Flask application.'
+    return 'Hello, World! This is a Flask application by nishant.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
